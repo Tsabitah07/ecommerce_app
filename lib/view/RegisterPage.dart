b8fe56ed8d5488d23ad96e21ba37899700ff0741
unknown <fawwaztr231@gmail.com>
2023-10-21 09:24:15 +0700

    feat [FT] fix responsive layout starting page

--- lib/view/RegisterPage.dart
+++ lib/view/RegisterPage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -7,116 +9,98 @@ import 'package:flutter_svg/flutter_svg.dart';
 class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xff8A83EF),
-      child: Stack(
-        children: [
-          Positioned(
-            left: 35,
-            right: 35,
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            color: Color(0xff8A83EF),
             child: SvgPicture.asset(
               'assets/images/Reg.svg',
-              width: 250,
-              height: 245,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * .5,
             ),
           ),
-          Positioned(
-            //Card
-            top: 250,
-            left: 0,
-            right: 0,
-            child: Container(
-              width: 280,
-              height: 544,
-              decoration: ShapeDecoration(
-                color: const Color(0xff292929),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color(0xff2A2A2A),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            width: MediaQuery.of(context).size.width,
+            height:
+                MediaQuery.of(context).size.height * .55, //fix bottom colors
+            child: Column(children: [
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                child: Text(
+                  "Let's get started!",
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-          ),
-          Positioned(
-            //sign up free card
-            top: 350,
-            left: 30,
-            right: 30,
-            child: Container(
-              width: 280,
-              height: 57,
-              decoration: ShapeDecoration(
-                color: Color(0xffC3DDD1),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
+              Container(
+                padding: EdgeInsets.all(18),
+                width: MediaQuery.of(context).size.width * .7,
+                decoration: BoxDecoration(
+                    color: Color(0xffC3DDD1),
+                    borderRadius: BorderRadius.circular(20)),
+                margin: EdgeInsets.only(top: 40),
+                child: Center(
+                  child: Text(
+                    "Sign up free",
+                    style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
-          ),
-          Positioned(
-            top: 270,
-            left: 60,
-            child: Text(
-              "Let's get Started!",
-              style: GoogleFonts.poppins(
-                color: Colors.white,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Positioned(
-            top: 365,
-            left: 140,
-            right: 120,
-            child: Text(
-              "Sign Up Free",
-              style: GoogleFonts.poppins(
-                color: Colors.black,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Positioned(
-            //continue with google card
-            top: 425,
-            left: 30,
-            right: 30,
-            child: Container(
-              width: 280,
-              height: 57,
-              decoration: ShapeDecoration(
-                color: const Color(0xff404452),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
+              Container(
+                padding: EdgeInsets.all(18),
+                width: MediaQuery.of(context).size.width * .7,
+                decoration: BoxDecoration(
+                    color: Color(0xff404452),
+                    borderRadius: BorderRadius.circular(20)),
+                margin: EdgeInsets.only(top: 40),
+                child: Center(
+                  child: Row(
+                    children: [
+                      SvgPicture.asset(
+                        "assets/images/Google.svg",
+                        height: 32,
+                        width: 32,
+                      ),
+                      Text(
+                        "Continue with google",
+                        style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
+              Container(
+                margin: EdgeInsets.all(25),
+                child: Text(
+                  "Login",
+                  style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              )
+            ]),
           ),
-          Positioned(
-            top: 436,
-            left: 50,
-            child: SvgPicture.asset(
-              'assets/images/Google.svg',
-              height: 32,
-              width: 32,
-            ),
-          ),
-          Positioned(
-            top: 440,
-            left: 95,
-            right: 70,
-            child: Text(
-              "Continue with google ",
-              style: GoogleFonts.poppins(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
