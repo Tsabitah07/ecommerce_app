f1a820f00af05517129d43c4bb49c208bf170c68
unknown <fawwaztr231@gmail.com>
2023-11-17 13:14:17 +0700

    feat [FT] adding detail layout

--- lib/view/registerPage.dart
+++ lib/view/registerPage.dart
@@ -1,12 +1,9 @@
-import 'package:ecommerce_app/view/HomePage.dart';
-import 'package:flutter/material.dart';
-
+import 'package:ecommerce_app/controller/registerController.dart';
 import 'package:ecommerce_app/themes/theme.dart';
 import 'package:ecommerce_app/widget/navigationButton.dart';
 import 'package:ecommerce_app/widget/textInput.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import '../controller/registerController.dart';
 
 class RegisterPage extends StatelessWidget {
   final controller = Get.put(RegisterController());
@@ -17,93 +14,60 @@ class RegisterPage extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: Stack(children: [
-        Align(
-          alignment: Alignment.topCenter,
-          child: Container(
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
               width: screenWidth,
-              height: screenHeight * .43,
-              color: Color(0xff8A83EF),
+              height: screenHeight * 0.43,
+              color: const Color(0xff8A83EF),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Obx(() => buildImagePreview(
-                      controller.imagePath.value, screenWidth * .25)),
+                      controller.imagePath.value, screenWidth * 0.25)),
                   InkWell(
                     onTap: () => controller.pickImage(),
-                    child: button("Add Image", primaryText, primaryColor),
+                    child: Button("Add Image", primaryText, primaryColor),
                   ),
                 ],
-              )),
-        ),
-        Align(
-            alignment: Alignment.topLeft,
-            child: InkWell(
-              child: Container(
-                  width: screenWidth,
-                  height: screenHeight * .43,
-                  color: Color(0xff8A83EF),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Obx(() => buildImagePreview(controller.imagePath.value, screenWidth * .25)),
-                      InkWell(
-                        onTap: () => controller.pickImage(),
-                        child: Button("Add Image", primaryText, primaryColor),
-                      ),
-                    ],
-                  )
               ),
-            )),
-        Align(
+            ),
+          ),
+          Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              height: MediaQuery.of(context).size.height * .61,
-              width: MediaQuery.of(context).size.width,
+              height: screenHeight * 0.61,
+              width: screenWidth,
               decoration: BoxDecoration(
-                  color: primaryText, borderRadius: BorderRadius.circular(20)),
+                color: primaryText,
+                borderRadius: BorderRadius.circular(20),
+              ),
               child: Column(
                 children: [
                   Container(
-                    margin: EdgeInsets.only(top: 19, bottom: 14),
+                    margin: const EdgeInsets.only(top: 19, bottom: 14),
                     child: Text(
-                      "Welcome-!!",
+                      'Welcome-!!',
                       style: Header,
                     ),
                   ),
-                )
+                  inputValue('Email', false),
+                  inputValue('Username', false),
+                  inputValue('Password', true),
+                  InkWell(
+                    onTap: () => controller.register(),
+                    child: Container(
+                      padding: const EdgeInsets.all(15),
+                      child: Button('REGISTER', primaryColor, primaryText),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: MediaQuery.of(context).size.height * .61,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                      color: primaryText,
-                      borderRadius: BorderRadius.circular(20)
-                  ),
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(top: 19, bottom: 14),
-                        child: Text("Welcome-!!",
-                          style: Header,
-                        ),
-                      ),
-                      inputValue("Email", false),
-                      inputValue("Username", false),
-                      inputValue("Password", true),
-                      InkWell(
-                        onTap: () => controller.register(),
-                        child: Container(
-                          padding: EdgeInsets.all(15),
-                            child: Button("REGISTER", primaryColor, primaryText)),
-                      )
-                    ],
-                  ),
-                )
-            )
-          ]
+          ),
+        ],
       ),
     );
   }
